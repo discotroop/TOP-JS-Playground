89e341935cb74bbd38f40e82246fd9e6a009cbeb
John Mark Goodson <john.mark.hat@gmail.com>
2019-09-05 15:21:22 -0400

    render now runs books.length times and pushes correct number of elements to DOM

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Library
+let handlers = {
+    getLibraryDiv: document.querySelector("#library"),
 
+}
+
+// Book constructor.
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,8 +17,33 @@ function Book(title, author, pages, read) {
 }
 
 let library = {
-    myLibrary: [],
+    books: ["this", "is", "a", "test", "me"],
+
     addBookToLibrary: function(title, author, pages, read) {
-        this.myLibrary.push(new Book(title, author, pages, read));
+        this.books.push(new Book(title, author, pages, read));
     },
+
+    render: function(libraryArray, libraryDOM) {
+        this.books.forEach(function(book) {
+            let libraryCard = document.createElement("p");
+            libraryCard.setAttribute("class", "libraryCard");
+            libraryCard.innerText = book;
+            console.log(libraryCard.innerText);
+            libraryDOM.appendChild(libraryCard);
+        })
+
+        // for (let i = 0; i < libraryArray.length; i++) {
+        //     console.log(i);
+        //     libraryDOM.appendChild(libraryCard.cloneNode());
+        // };
+        // let i = 0;
+        // while (i < libraryArray.length) {
+        //     libraryDOM.appendChild(libraryCard.cloneNode());
+        //     i++;
+        // }
+
+    }
 }
+library.addBookToLibrary("hucklyberry finn", "mark twain", "800", "read");
+
+library.render(library.books, handlers.getLibraryDiv)
