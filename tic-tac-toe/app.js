ca2dc1b262f39404089b74efaff6953724f6e67b
John Mark Goodson <john.mark.hat@gmail.com>
2019-09-11 18:46:51 -0400

    added gameBoard to JS and set functionality to draw board from gameboard.array

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,12 +1,35 @@
 let game = {
+    playerOne: {},
+    playerTwo: {},
+    
+    gameRound: function () {
+    
+
+    }
 
 }
 
-let gameboard = (function () {
-    let boardContainer = document.querySelector("div");
+let gameBoard = {
+    boardArray: ["","","","","","","","","",""],
+    populateBoard: function () {
+        let tiles = document.querySelectorAll("h3");
+        for (let i = 0; i < tiles.length; i++) {
+            tiles[i].setAttribute("data", i);
+        };
+        
+        tiles.forEach(function (tile) {
+            let index = (tile.attributes.data.value);
+            tile.innerText = gameBoard.boardArray[index].toUpperCase();
+        });
 
-})();
+        this.boardArray.length = 9;
+    }
 
+}
+
+gameBoard.populateBoard();
+
+// inner text.
 
 let player = {
 
